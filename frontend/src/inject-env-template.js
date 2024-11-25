ae1d3b4e9179dc71e4a80afc58fea37ac0d506dc
Harald Eggen <harald.eggen@gmail.com>
2024-11-25 13:38:54 +0100

    fix env name (#16)

--- frontend/src/inject-env-template.js
+++ frontend/src/inject-env-template.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-template-curly-in-string */
 window.injectEnv = {
-    AP_URL: "${API_URL}",
+    API_URL: "${API_URL}",
     API_SCOPE: "${API_SCOPE}",
     CLIENT_ID: "${CLIENT_ID}",
     TENANT_ID: "${TENANT_ID}",
