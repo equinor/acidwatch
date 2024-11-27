722e73ce0f2c43f8e9ed1f7e18ce6824a6819838
Fredrik Mellemstrand <fmell@equinor.com>
2024-11-27 16:18:48 +0100

    Implement basic api

---
+++ backend/tests/test_models_endpoints.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from acidwatch_api.app import app
+from acidwatch_api.authentication import authenticated_user_claims
+
+
+def override_authenticated_user_claims():
+    return {
+        "oid": "the_oid",
+        "upn": "theauthenticateduser@equinor.com",
+        "roles": [],
+    }
+
+
+app.dependency_overrides[authenticated_user_claims] = override_authenticated_user_claims
+
+
+def test_get_models():
+    client = TestClient(app)
+    response = client.get("/models")
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+    assert all("name" in m for m in response.json())
