dfbef1cd11b81370d4cd04fd1170da0e23dc077e
Daniel Vieira <kovauviske@poli.ufrj.br>
2025-01-24 15:16:08 +0100

    Create test_for_black_lint.py
    This is a fictional file to test the Black Lint Github Action.

---
+++ backend/tests/test_for_black_lint.py
@@ -0,0 +1,6 @@
+import numpy as np
+def sOmEth3(       x):
+  "A horribly made function to test Black Lint"
+  VAR= input("this is not a constant")
+  if 1==0:
+                      print(" a very weird indent")
