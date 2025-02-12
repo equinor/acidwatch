d6ff28a4e44f6eeb995d99a323ac51569f00eaf3
eq-sandkav <skav@equinor.com>
2025-02-12 16:53:11 +0100

    Separate ResultConcTable and ResultConcPlot

---
+++ frontend/src/components/ConcResultPlot.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import createPlotlyComponent from "react-plotly.js/factory";
+import { Typography } from "@equinor/eds-core-react";
+import { SimulationResults } from "../dto/SimulationResults";
+import Plotly from "plotly.js-basic-dist";
+import { extractPlotData } from "../functions/Formatting";
+
+const Plot = createPlotlyComponent(Plotly);
+
+interface ResultsProps {
+    simulationResults: SimulationResults;
+}
+
+const ResultConcPlot: React.FC<ResultsProps> = ({ simulationResults }) => {
+    return (
+        <div>
+            <Typography variant="h4">Change in concentrations</Typography>
+            <Plot
+                data={extractPlotData(simulationResults)}
+                layout={{ title: "" }}
+            />
+            <br></br>
+        </div>
+    );
+};
+
+export default ResultConcPlot;
+
+
