d1070e0fc1242e72831aa839610781e22220cc0c
eq-sandkav <skav@equinor.com>
2025-02-12 17:12:23 +0100

    Extract InputSettings to own component

--- frontend/src/pages/InputForm.tsx
+++ frontend/src/pages/InputForm.tsx
@@ -8,6 +8,7 @@ import { getModels, getProjects, runSimulation, saveResult, saveSimulation } fro
 import { useParams } from "react-router-dom";
 import { useErrorStore } from "../hooks/useErrorState";
 import { Project } from "../dto/Project";
+import InputSettings from "../components/InputSettings";
 
 interface InputConcentrations {
     [key: string]: number;
@@ -207,57 +208,7 @@ const InputForm: React.FC = () => {
                             <br />
                             <br />
                             <b>Settings</b>
-                            {Object.keys(formConfig.settings).map((key) => {
-                                const setting = formConfig.settings[key];
-                                return setting.input_type === "autocomplete" ? (
-                                    <Autocomplete
-                                        key={key}
-                                        id={key}
-                                        label={key}
-                                        meta={setting.meta}
-                                        placeholder={`Select ${key}`}
-                                        options={setting.values || []}
-                                        initialSelectedOptions={[setting.defaultvalue]}
-                                        hideClearButton={true}
-                                        onOptionsChange={({ selectedItems }) =>
-                                            setFormConfig((prevConfig: FormConfig) => ({
-                                                ...prevConfig,
-                                                settings: {
-                                                    ...prevConfig.settings,
-                                                    [key]: {
-                                                        ...prevConfig.settings[key],
-                                                        defaultvalue:
-                                                            selectedItems[0] || prevConfig.settings[key].defaultvalue,
-                                                    },
-                                                },
-                                            }))
-                                        }
-                                    />
-                                ) : (
-                                    <TextField
-                                        type="number"
-                                        key={key}
-                                        label={key}
-                                        id={key}
-                                        step="any"
-                                        name={key}
-                                        meta={setting.meta}
-                                        value={setting.defaultvalue}
-                                        onChange={(e: { target: { value: string } }) =>
-                                            setFormConfig((prevConfig: FormConfig) => ({
-                                                ...prevConfig,
-                                                settings: {
-                                                    ...prevConfig.settings,
-                                                    [key]: {
-                                                        ...prevConfig.settings[key],
-                                                        defaultvalue: parseFloat(e.target.value),
-                                                    },
-                                                },
-                                            }))
-                                        }
-                                    />
-                                );
-                            })}
+                            <InputSettings formConfig={formConfig} setFormConfig={setFormConfig} />
                         </div>
                     )}
                     <br />
