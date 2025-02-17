02a0a5bd3e53a8f7d05f59abeb2aa0ec010bd02b
Harald Eggen <haegg@equinor.com>
2025-02-17 15:23:09 +0100

    Get usertoken

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,4 +1,4 @@
-import { BrowserCacheLocation, InteractionRequiredAuthError, PublicClientApplication } from "@azure/msal-browser";
+import { AccountInfo, BrowserCacheLocation, InteractionRequiredAuthError, IPublicClientApplication, PublicClientApplication } from "@azure/msal-browser";
 import { Providers, Msal2Provider } from "@microsoft/mgt";
 import config from "../configuration";
 
@@ -21,10 +21,11 @@ Providers.globalProvider = new Msal2Provider({
     scopes: ["User.Read", "People.Read", "User.ReadBasic.All"],
 });
 
-export async function getAccessToken(): Promise<string | null> {
+export async function getAccessToken(scope?: string): Promise<string | null> {
+    const scopes = [scope || config.API_SCOPE];
     try {
         const tokenResponse = await msalInstance.acquireTokenSilent({
-            scopes: [config.API_SCOPE],
+            scopes: scopes,
             forceRefresh: false,
         });
         return tokenResponse.accessToken;
@@ -32,12 +33,43 @@ export async function getAccessToken(): Promise<string | null> {
         console.error(error);
         if (error instanceof InteractionRequiredAuthError) {
             await msalInstance.acquireTokenRedirect({
-                scopes: [config.API_SCOPE],
+                scopes: scopes,
             });
             return null;
         }
         return null;
     }
+}
+
+import { useMsal, useAccount } from "@azure/msal-react";
+
+export async function getusertoken(account: AccountInfo|null, instance: IPublicClientApplication) {
+    const scope = "d2e2c318-b49a-4174-b4b4-256751558dc5/.default";
+    console.log("Account: ", account);
+    const requestToken = async () => {
+        if (account) {
+            const request = {
+                scopes: [scope],
+                account: account,
+            };
+            try {
+                const response = await instance.acquireTokenSilent(request);
+                const accessToken = response.accessToken;
+                // Use accessToken to call your API
+            } catch (error) {
+                if (error instanceof Error) {
+                    if (error.name === "InteractionRequiredAuthError") {
+                        // Fallback to interaction when silent request fails
+                        instance.acquireTokenRedirect(request);
+                    }
+                } else {
+                    // Handle other errors
+                    console.error(error);
+                }
+            }
+        }
+    };
+    return await requestToken();
 }
 
 await msalInstance.initialize();
