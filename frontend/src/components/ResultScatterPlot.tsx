aa2769ade7a1d71e3e062261229b7193b03c5ad6
eq-sandkav <skav@equinor.com>
2025-02-20 13:55:30 +0100

    Change Line- to ScatterPlot

--- frontend/src/components/ResultScatterPlot.tsx
+++ frontend/src/components/ResultScatterPlot.tsx
@@ -1,47 +1,39 @@
+import React from "react";
+import { XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ScatterChart, Scatter, ZAxis } from "recharts";
+import { getDistributedColor } from "../functions/Colors";
 
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ScatterChart, Scatter, Brush } from 'recharts';
+interface IGraphData {
+    compound: string;
+    conc: number;
+    label: string;
+}
 
-const TestGraph: React.FC = () => {
-    const data = [
-        { name: "H2S", Arcs: 40, LabData: 55 },
-        { name: "H2SO4", Arcs: 80, LabData: 50 },
-        { name: "H2CO3", Arcs: 90, LabData: 91 },
-        { name: "H2O", Arcs: 35, LabData: 30 },
-        { name: "NO3", Arcs: 10, LabData: 0 },
-        { name: "CO2", Arcs: 10, LabData: 30},
-        { name: "HCl", Arcs: 100, LabData: 60 },
-        { name: "SCO", Arcs: 40, LabData: 20 },
-      ];
+interface IResultScatterGraph {
+    GraphInput: IGraphData[];
+}
 
-      const [activeSeries, setActiveSeries] = React.useState<Array<string>>([]);
-
-      const handleLegendClick = (dataKey: string) => {
-        if (activeSeries.includes(dataKey)) {
-          setActiveSeries(activeSeries.filter(el => el !== dataKey));
-        } else {
-          setActiveSeries(prev => [...prev, dataKey]);
-        }
-      };
+const ResultScatterGraph: React.FC<IResultScatterGraph> = ({ GraphInput }) => {
 
     return (
-        <div >
-            <div style={{ width: '600px' }}>
+        <div>
+            <div style={{ width: "600px" }}>
                 <ResponsiveContainer width="100%" height={300}>
-                    <LineChart data={data} >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    {Object.keys(data[0]).filter((item) => item!=="name").map((item: string) => {
-                        const color = item === "Arcs" ? "blue":"red";
-                        return <Line hide={activeSeries.includes(item)} type="monotone" dataKey={item} stroke="none" fill={color} />
-                    })}
-                    <Legend height={36} verticalAlign='top' iconType="circle" onClick={props => handleLegendClick(String(props.dataKey))} />
-                    </LineChart>
+                    <ScatterChart>
+                        <CartesianGrid />
+                        <XAxis
+                            dataKey="compound"
+                            type="category"
+                            allowDuplicatedCategory={false}
+                            tick={{ fontSize: 14 }}
+                        />
+                        <YAxis dataKey="conc" />
+                        <ZAxis range={[200]} /> {/* Size of dots */}
+                        <Tooltip />
+                        <Scatter data={GraphInput} />
+                    </ScatterChart>
                 </ResponsiveContainer>
             </div>
         </div>
-    )      
-}
-export default TestGraph
+    );
+};
+export default ResultScatterGraph;
