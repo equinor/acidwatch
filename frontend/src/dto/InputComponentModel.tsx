0142f785ede5bd55736c9c77fcc34f95df8ca3fb
eq-sandkav <skav@equinor.com>
2025-03-20 11:55:09 +0100

    Implement first Batch Input
    This should allow users to select ranges with certain steps for multiple individual components.
    This was suggested by users as an essential feature for "what if?" experiments.

---
+++ frontend/src/dto/InputComponentModel.tsx
@@ -0,0 +1,7 @@
+
+export interface InputComponentProps {
+    conc: number,
+    from: number,
+    to: number,
+    step: number,
+}
--- frontend/tests/Functions.test.tsx
+++ frontend/tests/Functions.test.tsx
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest"
 import {convertToSubscripts} from "../src/functions/Formatting"
 import { extractAndReplaceKeys} from "../src/api/api"
 import React from "react";
+import { calculateNumberOfSimulations } from "../src/functions/Calculations"
 
 describe("convertToSubscripts", () => {
     it("should return empty div when provided an empty string", () => {
@@ -53,5 +54,100 @@ describe("extractAndReplaceKeys", () => {
         }
 
         expect(expectedOutput).toEqual(res);
+    })
+})
+
+describe("calculateNumberOfSimulations", () => {
+    it("should only predict 1 run given no selected components", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 2, step: 1},
+            "c2": {conc:1, from: 1, to: 2, step: 1},
+            "c3": {conc:1, from: 1, to: 2, step: 1},
+        }
+        const selectedComponents = new Set([])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(1)
+    })
+
+    it("should only count selected components", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 3, step: 1},
+            "c2": {conc:1, from: 1, to: 2, step: 1},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(3)
+    })
+
+    it("should predict exponentially demanding runs", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 100, step: 1},
+            "c2": {conc:1, from: 1, to: 100, step: 1},
+            "c3": {conc:1, from: 1, to: 100, step: 1},
+        }
+        const selectedComponents = new Set(["c1","c2","c3"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(1000000)
+    })
+
+    it("should handle decreasing ranges", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 3, to: 1, step: 1},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(3)
+    })
+
+    it("should predict range 0 to produce 1 simulation", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 1, step: 1},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(1)
+    })
+
+    it("should handle decimal steps", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 2, step: 0.5},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(3) // 1.0 , 1.5 , 2.0
+    })
+
+    it("should properly predict simulations when range is not divisible by step (undershooting)", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 2, step: 0.49},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(3) // 1 , 1.49 , 1.98
+    })
+
+    it("should properly predict simulations when range is not divisible by step (overshooting)", () => {
+        const componentConcs = {
+            "c1": {conc:1, from: 1, to: 2, step: 0.51},
+        }
+        const selectedComponents = new Set(["c1"])
+
+        const res = calculateNumberOfSimulations(componentConcs,selectedComponents)
+
+        expect(res).toBe(2) // 1 , 1.51
     })
 })
