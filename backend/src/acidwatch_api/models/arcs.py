45946e8a0fd0b5211793c4ab983ddf7079d6fa04
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-06-02 14:19:36 +0200

    Remove authentication requirement from running arcs

--- backend/src/acidwatch_api/models/arcs.py
+++ backend/src/acidwatch_api/models/arcs.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from pydantic import BaseModel, Field
 from typing import Annotated
 from acidwatch_api import configuration
-from acidwatch_api.authentication import acquire_token_for_downstream_api, oauth2_scheme
 from acidwatch_api.models.datamodel import SimulationRequest, SimulationResults
 
 router = APIRouter()
@@ -32,7 +31,6 @@ def convert_to_arcs_simulation_request(
 @router.post("/runs")
 async def post_arcs_run(
     simulation_request: SimulationRequest,
-    jwt_token: Annotated[str, oauth2_scheme],
 ) -> SimulationResults:
     arcs_simulation_request = convert_to_arcs_simulation_request(simulation_request)
     async with httpx.AsyncClient() as client:
@@ -40,10 +38,6 @@ async def post_arcs_run(
             f"{configuration.ARCS_API_BASE_URI}/run_simulation",
             json=arcs_simulation_request.model_dump(),
             timeout=300.0,
-            headers={
-                "Authorization": "Bearer "
-                + acquire_token_for_downstream_api(MODEL, jwt_token)
-            },
         )
 
     res.raise_for_status()
