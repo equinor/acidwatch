d0690f7ac451914255ada09f91b640e8cd06d459
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-06-02 14:20:47 +0200

    Remove MSAL authentication template from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
 import Dashboard from "./pages/Home";
