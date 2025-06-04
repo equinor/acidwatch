d1e989df4177b863e566a0d9c5d0aac5e9445684
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-06-04 14:10:36 +0200

    Refactor save button

---
+++ frontend/src/components/SaveButtonComponent.tsx
@@ -0,0 +1,29 @@
+﻿import { Button } from "@equinor/eds-core-react";
+
+const SaveButtonComponent: React.FC<{
+    handleSave: () => void;
+    isSimulationSaved: boolean;
+    simulationName: string;
+    selectedProjectId: string;
+    isSimulationSaving: boolean;
+}> = ({ handleSave, isSimulationSaved, simulationName, selectedProjectId, isSimulationSaving }) => {
+    const notSaveAble = !Boolean(selectedProjectId && simulationName && simulationName.trim()) && !isSimulationSaving;
+
+    return (
+        <Button
+            onClick={handleSave}
+            disabled={notSaveAble}
+            variant={!Boolean(selectedProjectId && simulationName) ? "ghost" : "contained"}
+        >
+            {isSimulationSaved
+                ? `Saved simulation as "${simulationName}" to project "${selectedProjectId}"`
+                : !selectedProjectId
+                ? "Cannot save without a project"
+                : !simulationName || !simulationName.trim()
+                ? "Cannot save without a valid simulation name"
+                : "Save simulation"}
+        </Button>
+    );
+};
+
+export default SaveButtonComponent;
