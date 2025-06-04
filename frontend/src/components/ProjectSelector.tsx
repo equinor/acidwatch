37326fcc1440ccf42aa6f56e28c989fb71b81f4f
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-06-04 08:52:37 +0200

    Add functional component test

---
+++ frontend/src/components/ProjectSelector.tsx
@@ -0,0 +1,21 @@
+﻿import { Autocomplete, TextField } from "@equinor/eds-core-react";
+
+interface ProjectSelectorProps {}
+
+const ProjectSelector: React.FC<ProjectSelectorProps> = ({}) => {
+    return (
+        <>
+            <TextField
+                label="Save to project:"
+                placeholder={"ProjectName"}
+                disabled={false}
+                id = {"model"}
+                //onOptionsChange={({ selectedItems }) => {
+                //    setSelectedProjectId(projects!.find((proj) => proj.name === selectedItems[0])?.id || "");
+                //    setIsSimulationSaved(false);
+                //}}
+            />{" "}
+        </>
+    );
+};
+export default ProjectSelector;
