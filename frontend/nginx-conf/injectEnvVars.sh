a01990e925619b4c624188659b4fcd9a3f64da55
Harald Eggen <haegg@equinor.com>
2025-06-05 14:29:40 +0200

    add logging for nginx startup for debug in radix

--- frontend/nginx-conf/injectEnvVars.sh
+++ frontend/nginx-conf/injectEnvVars.sh
@@ -1,4 +1,5 @@
 envsubst < /app/www/inject-env-template.js > /app/www/inject-env.js
 envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 echo $(cat /app/www/inject-env.js)
+echo $(cat /etc/nginx/conf.d/default.conf)
 echo $(date) Starting Nginx… env: ${ACIDWATCH_ENVIRONMENT}
