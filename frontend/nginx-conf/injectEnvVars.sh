696b95074325463d491969b571c4cfb5375f5688
Harald Eggen <haegg@equinor.com>
2025-06-05 14:38:33 +0200

    debug nginx

--- frontend/nginx-conf/injectEnvVars.sh
+++ frontend/nginx-conf/injectEnvVars.sh
@@ -1,5 +1,7 @@
 envsubst < /app/www/inject-env-template.js > /app/www/inject-env.js
 envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 echo $(cat /app/www/inject-env.js)
-echo $(cat /etc/nginx/conf.d/default.conf)
+echo "===== Rendered /etc/nginx/conf.d/default.conf ====="
+cat /etc/nginx/conf.d/default.conf
+echo "===================================================="
 echo $(date) Starting Nginx… env: ${ACIDWATCH_ENVIRONMENT}
