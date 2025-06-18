971562ae416d64f329859d50d892d0b9c07a6a32
Harald Eggen <haegg@equinor.com>
2025-06-18 10:28:05 +0200

    Set active color on sidebar menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
 import Dashboard from "./pages/Home";
@@ -14,19 +15,15 @@ import HelpPage from "./pages/HelpPage";
 const Main = styled.div`
     display: flex;
     margin-top: 55px;
+    height: calc(100vh - 55px);
 `;
 
 const Content = styled.div`
-    position: fixed;
-    top: 50px;
-    left: 70px;
-    right: 0;
-    bottom: 0;
+    flex: 1;
     padding: 30px;
     overflow-y: auto;
-    display: "flex";
-    height: "400px";
-    backgroundcolor: "#DEECEE";
+    #background-color: #deecee;
+    min-width: 0;
 `;
 
 const App: React.FC = () => {
