25be5ed29f7770f216977189fcf8674c85f4cbd2
Harald Eggen <haegg@equinor.com>
2025-06-19 22:12:52 +0200

    Moved styling to one location and rearrange

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import { Divider, SideBar as EDS_SideBar, SidebarLinkProps } from "@equinor/eds-core-react";
 import { NavLink } from "react-router-dom";
 import { home, launch, opacity } from "@equinor/eds-icons";
-
-const SidebarContainer = styled.div`
-    top: 50px;
-    height: calc(100vh - 50px);
-    transition: width 0.3s;
-`;
 
 const StyledNavLink = ({ to, children, ...rest }: any) => {
     return (
@@ -42,17 +35,15 @@ const sidemenuItems: CustomSidebarLinkProps[] = [
 
 const SideBar: React.FC = () => {
     return (
-        <SidebarContainer>
-            <EDS_SideBar>
-                <EDS_SideBar.Content>
-                    <Divider size="2" color="light" style={{ marginBlockEnd: 0 }} />
-                    {sidemenuItems.map((m) => (
-                        <EDS_SideBar.Link key={m.label} as={StyledNavLink} to={m.path} {...m} />
-                    ))}
-                </EDS_SideBar.Content>
-                <EDS_SideBar.Toggle />
-            </EDS_SideBar>
-        </SidebarContainer>
+        <EDS_SideBar>
+            <EDS_SideBar.Content>
+                <Divider size="2" color="light" style={{ marginBlockEnd: 0 }} />
+                {sidemenuItems.map((m) => (
+                    <EDS_SideBar.Link key={m.label} as={StyledNavLink} to={m.path} {...m} />
+                ))}
+            </EDS_SideBar.Content>
+            <EDS_SideBar.Toggle />
+        </EDS_SideBar>
     );
 };
 
