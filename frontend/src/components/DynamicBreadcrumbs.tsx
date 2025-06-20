28ca781dc8fc61645c14af2d2f6ba823f7089eca
Harald Eggen <haegg@equinor.com>
2025-06-20 11:12:42 +0200

    Add basic first version breadcrumb

---
+++ frontend/src/components/DynamicBreadcrumbs.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useLocation, Link, useMatch } from "react-router-dom";
+import { Breadcrumbs } from "@equinor/eds-core-react";
+
+const DynamicBreadcrumbs: React.FC = () => {
+    const projectMatch = useMatch("/project/:projectId/*");
+    const projectId = projectMatch?.params?.projectId;
+    const simulationMatch = useMatch("/project/:projectId/simulation/:simulationId");
+    const simulationId = simulationMatch?.params?.simulationId;
+    const location = useLocation();
+
+    const [projectName, setProjectName] = useState<string>("");
+    const [simulationName, setSimulationName] = useState<string>("");
+
+    const getBreadcrumbs = () => {
+        const pathSegments = location.pathname.split("/").filter(Boolean);
+        const breadcrumbs = [];
+
+        if (pathSegments.includes("project")) {
+            breadcrumbs.push(
+                <Breadcrumbs.Breadcrumb key="project" as={Link} to={`/project/${projectId}`}>
+                    {projectName || `${(projectId ?? "").slice(0, 8)}`}
+                </Breadcrumbs.Breadcrumb>
+            );
+        }
+        if (pathSegments.includes("simulation") && simulationId) {
+            breadcrumbs.push(
+                <Breadcrumbs.Breadcrumb
+                    key="simulation"
+                    as={Link}
+                    to={`/project/${projectId}/simulation/${simulationId}`}
+                    aria-current="page"
+                >
+                    {simulationName || `${(simulationId ?? "").slice(0, 8)}`}
+                </Breadcrumbs.Breadcrumb>
+            );
+        }
+
+        return breadcrumbs;
+    };
+
+    return <Breadcrumbs>{getBreadcrumbs()}</Breadcrumbs>;
+};
+
+export default DynamicBreadcrumbs;
