74eb4b4c0ee283c778e71f452458bd48cbaaeda1
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-06-27 13:18:58 +0000

    Point to arcs dev instead of cloning repository

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -8,7 +8,7 @@ poetry install --directory ./backend
 
 echo "CO2SPEC_API_SCOPE=DUMMY_SCOPE">>./backend/.env
 echo "CO2SPEC_API_BASE_URI=DUMMY_URL">>./backend/.env
-echo "ARCS_API_BASE_URI=https://$CODESPACE_NAME-8000.app.github.dev">>./backend/.env
+echo "ARCS_API_BASE_URI=https://api-arcs-dev.radix.equinor.com">>./backend/.env
 echo "FRONTEND_URI=https://$CODESPACE_NAME-5173.app.github.dev">>./backend/.env
 
 deactivate 
@@ -17,15 +17,4 @@ echo "VITE_API_URL=https://$CODESPACE_NAME-8001.app.github.dev">>./frontend/.env
 
 npm install ./frontend
 
-git clone -b main https://github.com/equinor/arcs.git
-
-cd arcs
-
-git lfs pull
-
-python3 -m venv venv
-
-source venv/bin/activate
-
-pip install .
 
