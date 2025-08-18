20afce8d8d5c7e3068cec7ae0d2e33d594977867
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-08-18 15:54:25 +0200

    Add accordions for model descriptions

---
+++ frontend/src/components/TooltipIcon.tsx
@@ -0,0 +1,10 @@
+﻿import { Icon, Tooltip } from "@equinor/eds-core-react";
+import { info_circle } from "@equinor/eds-icons";
+
+export const TooltipIcon = ({ tooltip }: { tooltip: string }) => {
+    return (
+        <Tooltip title={tooltip} enterDelay={700} style={{ whiteSpace: "pre-wrap" }}>
+            <Icon data={info_circle} size={24} style={{ paddingLeft: 8 }} />
+        </Tooltip>
+    );
+};
