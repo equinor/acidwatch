794d6b71af965ae06a1615b275e332996739e6fe
Muhammad Usama <110888193+UsamaEquinorAFK@users.noreply.github.com>
2025-09-05 15:03:39 +0200

    Seperate out filtering functions from components
    These functions are used in ploting logic for lab results and simulation results

---
+++ frontend/src/functions/Filtering.tsx
@@ -0,0 +1,22 @@
+﻿import { ExperimentResult } from "../dto/ExperimentResult";
+import { ModelConfig } from "../dto/FormConfig";
+import { ScatterGraphData } from "../dto/ScatterGraphInput";
+
+export const filterValidModels = (experiment: ExperimentResult, models: ModelConfig[]) => {
+    const filteredConcs = Object.fromEntries(
+        Object.entries(experiment.initialConcentrations).filter(([, value]) => Number(value) !== 0)
+    );
+    return models
+        .filter((model) => model.category === "Primary")
+        .filter((model) => Object.entries(filteredConcs).every(([key]) => model.validSubstances.includes(key)));
+};
+
+export const filterGraphDataByComponents = (
+    graphData: ScatterGraphData[],
+    selectedComponents: string[]
+): ScatterGraphData[] => {
+    if (selectedComponents.length === 0) {
+        return [];
+    }
+    return graphData.filter((dataPoint) => selectedComponents.includes(dataPoint.x as string));
+};
