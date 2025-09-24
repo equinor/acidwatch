8aec7365477145c1611b482634c2b58bdcec7c75
Zohar Malamant <ZOM@equinor.com>
2025-09-24 16:05:36 +0200

    Allow substances with parens, eg. HC(CH3)3

--- backend/tests/models/test_all_adapters.py
+++ backend/tests/models/test_all_adapters.py
@@ -4,7 +4,7 @@ import re
 from acidwatch_api.models.base import ADAPTERS, BaseAdapter
 
 
-SUBSTANCE_PATTERN = re.compile(
+ATOM_PATTERN = (
     "(A[cglmrstu]"
     "|B[aehikr]?"
     "|C[adeflmnorsu]?"
@@ -32,13 +32,70 @@ SUBSTANCE_PATTERN = re.compile(
 )
 
 
+TOKENS = [
+    ("ATOM", ATOM_PATTERN),
+    ("OPEN", "\\("),
+    ("CLOSE", "\\)(\\d*)"),
+    ("INVALID", "."),
+]
+
+
+SUBSTANCE_PATTERN = re.compile(
+    "|".join(f"(?P<{name}>{pattern})" for name, pattern in TOKENS)
+)
+
+
 def _assert_is_valid_substance(substance: str) -> None:
+    assert substance
+
+    groupdepth = 0
     for match in SUBSTANCE_PATTERN.finditer(substance):
-        if match[2]:
-            atom = int(match[2])
-            assert atom > 0, (
-                f"In {substance}, element {match[1]} must have non-zero atoms"
-            )
+        kind = match.lastgroup
+
+        if kind == "ATOM":
+            name = match[2]
+            atom = int(match[3] or "1")
+
+            assert atom > 0, f"In {substance}, element {name} must have non-zero atoms"
+        elif kind == "OPEN":
+            groupdepth += 1
+        elif kind == "CLOSE":
+            mult = int(match[6] or "1")
+            groupdepth -= 1
+            assert mult > 1, f"In {substance}, group multiplier must be greater than 1"
+        else:
+            raise AssertionError(f"Unexpected character '{match[0]}'")
+
+    assert groupdepth == 0, f"In {substance}, groups must be closed"
+
+
+@pytest.mark.parametrize(
+    "substance",
+    [
+        "H2O",
+        "H2",
+        "CH(CH3)3",
+    ],
+)
+def test_sanity_check_good(substance):
+    _assert_is_valid_substance(substance)
+
+
+@pytest.mark.parametrize(
+    "substance",
+    [
+        "",
+        "H0",
+        "water",
+        "1234",
+        "CH(",
+        "CH(CH3)0",
+        "CH(CH3)",
+    ],
+)
+def test_sanity_check_bad(substance):
+    with pytest.raises(AssertionError):
+        _assert_is_valid_substance(substance)
 
 
 @pytest.mark.parametrize("adapter", ADAPTERS.values())
