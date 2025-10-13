9d5f8e50d6f5f3ce7b728196a0a258f34c1b31b6
Harald Eggen <haegg@equinor.com>
2025-10-13 15:47:54 +0200

    Move oasis calls to backend

--- frontend/nginx-conf/injectEnvVars.sh
+++ frontend/nginx-conf/injectEnvVars.sh
@@ -1,5 +1,4 @@
 envsubst < /app/www/inject-env-template.js > /app/www/inject-env.js
-envsubst '${OASIS_URL} ${OASIS_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 echo "===== Rendered /etc/nginx/conf.d/default.conf ====="
 cat /etc/nginx/conf.d/default.conf
 echo "===================================================="
