6ae9eeb49ca0048d4b2c559b5db4b5fbec35baa4
Lars Petter Hauge <larsenhauge@gmail.com>
2026-01-16 14:42:54 +0100

    Remove unused old data structures

--- backend/src/acidwatch_api/models/datamodel.py
+++ backend/src/acidwatch_api/models/datamodel.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime
-from typing import Any, List, Literal, Optional, Dict, TypeAlias, Iterable
-from uuid import UUID, uuid4
+from typing import Any, Literal, Optional, Dict, TypeAlias, Iterable
 
 from pydantic import BaseModel, ConfigDict, Field
 from pydantic.alias_generators import to_camel
@@ -98,49 +96,3 @@ class ChartData(BaseModel):
     values: Dict[str, float]
     variance: Dict[str, float]
     variance_minus: Dict[str, float]
-
-
-class SimulationRequest(BaseModel):
-    model_config = ConfigDict(
-        alias_generator=to_camel,
-        populate_by_name=True,
-        from_attributes=True,
-    )
-    initial_concentrations: dict[str, float] = Field(default_factory=dict)
-    parameters: dict[str, float] = Field(default_factory=dict)
-
-
-class Project(BaseModel):
-    id: UUID = uuid4()
-    name: str = ""
-    description: str = ""
-    owner: str = ""
-    owner_id: str = ""
-    private: bool = True
-    access_ids: List[str] = []
-    date: str = Field(default_factory=lambda: datetime.utcnow().isoformat())
-
-
-class Scenario(BaseModel):
-    model_config = ConfigDict(
-        alias_generator=to_camel,
-        populate_by_name=True,
-        from_attributes=True,
-    )
-    id: UUID = Field(default_factory=lambda: uuid4())
-    project_id: str = ""
-    name: str = ""
-    owner: str = ""
-    scenario_inputs: SimulationRequest
-    model: str = "arcs"
-    date: str = Field(default_factory=lambda: datetime.utcnow().isoformat())
-
-
-# TODO: define the Result model, this is what we store in db
-class Result(BaseModel):
-    id: UUID = Field(default_factory=lambda: uuid4())
-    scenario_id: str = ""
-    raw_results: str = ""
-    initial_concentrations: Optional[dict[str, float]] = Field(default_factory=dict)
-    final_concentrations: Optional[dict[str, float]] = Field(default_factory=dict)
-    panels: Optional[List[AnyPanel]] = Field(default_factory=list)
