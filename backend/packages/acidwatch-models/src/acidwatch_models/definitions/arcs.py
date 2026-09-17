89bbd7bb1fd919950023ec2b8983617ec85b68b5
Benjamin Williamson <benjamin.williamson.10@ucl.ac.uk>
2026-09-17 14:29:35 +0200

    update backend/packages/acidwatch-models/src/acidwatch_models/definitions/arcs.py and arcs_exp.py
    add a flow chart found in frontend/public/images/outer_loop_arcs.png
    
    change arcs_exp's display name to ARCS 1.5.1 and arcs's display name is no ARCS 1.0.0 (Legacy) (filenames etc. are still arcs and arcs_exp )

--- backend/packages/acidwatch-models/src/acidwatch_models/definitions/arcs.py
+++ backend/packages/acidwatch-models/src/acidwatch_models/definitions/arcs.py
@@ -32,12 +32,22 @@ class ArcsAdapter(BaseAdapter):
     model_id = "arcs"
     display_name = "ARCS"
     description = """\
-Automated Reactions for CO2 Storage (ARCS) model.
+### Legacy Version of Automated Reactions for CO<sub>2</sub> Storage (ARCS)
 
-ARCS combines first-principles calculations with Monte-Carlo sampling and
-models possible reactions that may occur under a given set of conditions.
-This process identifies the most frequently occurring reactions and paths,
-final products, and expected concentrations.
+** The latest version can be found under ARCS 1.5.1 **
+
+```
+ARCS 1.5.1 includes: 
+    - coupled cluster _ab initio_ calculated database
+    - more compounds (44 compounds) 
+    - faster algorithm
+```
+
+ARCS is a Monte-Carlo sampled large equilibrium reaction network.
+
+The reaction network is made up of all possible balanced equilibrium reactions using first-principles calculations (using density functional theory with the [VASP code](https://vasp.at), SCAN functional) to form Gibbs Free energies and equilibrium constants as a function of temperature and pressure.
+
+The Monte-Carlo sampling utilises a random walk over the reaction network to identify the most frequently occuring reactions and updates the input concentrations accordingly.
 
 Source code found [on GitHub (equinor/arcs)](https://github.com/equinor/arcs/tree/21ded96960d28d549c0950fbc1aa09c94159f652).
 """
