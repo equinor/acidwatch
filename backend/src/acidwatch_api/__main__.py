69e362cf87a28e9b0b170264fac43ef27b3ed232
Lars Petter Hauge <larsenhauge@gmail.com>
2026-09-23 10:15:23 +0200

    Be consistent with backend ports
    We have used 8001 in readme and for local docker, and 8000 for
    production environment. Likely just a weird incident. Let's stick with one.

--- backend/src/acidwatch_api/__main__.py
+++ backend/src/acidwatch_api/__main__.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("acidwatch_api.app:app", host="localhost", port=8001, reload=True)
+    uvicorn.run("acidwatch_api.app:app", host="localhost", port=8000, reload=True)
